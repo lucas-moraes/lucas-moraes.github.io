b82cbe8b86fed3ec4bb068c04d146508bf153cf3
Lucas Moraes <lucas.moraes88@yahoo.com>
2025-12-16 23:25:26 -0300

    feat: implement translation

---
+++ js/i18n.js
@@ -0,0 +1,37 @@
+import { translations } from "./translations.js";
+
+const DEFAULT_LANG = "br";
+const buttonTititle = document.getElementById("i18n-button-text");
+
+let currentLang = window.localStorage.getItem("lang") ?? DEFAULT_LANG;
+
+function SetLang(lang) {
+  currentLang = lang;
+  window.localStorage.setItem("lang", lang);
+  buttonTititle.textContent = lang === "en" ? "PT-BR" : "EN-US";
+
+  document.querySelectorAll("[data-i18n-key]").forEach((el) => {
+    const key = el.getAttribute("data-i18n-key");
+    const translation = translations[currentLang][key];
+
+    if (translation) {
+      if (el.placeholder !== undefined) {
+        el.placeholder = translation;
+      }
+      if (el.title !== undefined) {
+        el.title = translation;
+      }
+
+      el.textContent = translation;
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  SetLang(currentLang);
+
+  document.getElementById("lang-select").addEventListener("click", () => {
+    const newLang = currentLang === "en" ? "br" : "en";
+    SetLang(newLang);
+  });
+});
---
+++ js/translations.js
@@ -0,0 +1,31 @@
+export const translations = {
+  en: {
+    "professional-title": "Full Stack Developer",
+    "professional-experience": "years of experience",
+    "title-projects": "Projects",
+    "button-text-access": "Access",
+    "project-type-whatzator": "Chrome browser extension",
+    "project-description-whatzator":
+      "Allows you to start conversations on WhatsApp Web without having to save the number in your contact list. Developed using HTML, CSS, and JavaScript, the extension offers an intuitive and easy-to-use interface, allowing users to quickly send messages to any phone number.",
+    "project-type-pjtizator": "CLT vs. PJ calculator website",
+    "project-description-pjtizator": "Comparative simulator between CLT and PJ contracts for IT service providers.",
+    "project-type-articles": "Texts to reinforce knowledge",
+    "project-description-articles":
+      "Website dedicated to sharing technical texts and summaries on various technologies and development concepts.",
+  },
+  br: {
+    "professional-title": "Desenvolvedor Full Stack",
+    "professional-experience": "anos de experiência",
+    "title-projects": "Projetos",
+    "button-text-access": "Acessar",
+    "project-type-whatzator": "Extensão para navegador chrome",
+    "project-description-whatzator":
+      "Permite iniciar conversas no WhatsApp Web sem a necessidade de salvar o número na lista de contatos. Desenvolvida utilizando html, css e javascript, a extensão oferece uma interface intuitiva e fácil de usar, permitindo que os usuários enviem mensagens rapidamente para qualquer número de telefone.",
+    "project-type-pjtizator": "Site calculadora CLT x PJ",
+    "project-description-pjtizator":
+      "Simulador comparativo entre contratos CLT e PJ para prestadores de serviços de TI.",
+    "project-type-articles": "Textos para fixar conhecimentos",
+    "project-description-articles":
+      "Site dedicado a compartilhar textos técnicos e resumos sobre diversas tecnologias e conceitos de desenvolvimento.",
+  },
+};
