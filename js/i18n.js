a2267f1068b980a5be91ef398311eaae4564c6df
Lucas Moraes <lucas.moraes88@yahoo.com>
2026-03-31 17:02:42 -0300

    feat: dark theme

--- js/i18n.js
+++ js/i18n.js
@@ -1,9 +1,17 @@
 import { translations } from "./translations.min.js";
 
 const DEFAULT_LANG = "en";
+const DEFAULT_THEME = "light";
 const buttonTititle = document.getElementById("i18n-button-text");
 
 let currentLang = window.localStorage.getItem("lang") ?? DEFAULT_LANG;
+let currentTheme = window.localStorage.getItem("theme") ?? DEFAULT_THEME;
+
+function SetTheme(theme) {
+  currentTheme = theme;
+  window.localStorage.setItem("theme", theme);
+  document.documentElement.setAttribute("data-theme", theme);
+}
 
 function SetLang(lang) {
   currentLang = lang;
@@ -28,10 +36,30 @@ function SetLang(lang) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  SetTheme(currentTheme);
   SetLang(currentLang);
+
+
 
   document.getElementById("i18n-button-text").addEventListener("click", () => {
     const newLang = currentLang === "en" ? "br" : "en";
     SetLang(newLang);
+  });
+
+  const themeToggleButton = document.getElementById("theme-toggle");
+  themeToggleButton.textContent = currentTheme === "light" ? "🔆" : "🌙";
+
+  themeToggleButton.addEventListener("click", () => {
+    let newTheme;
+    if(currentTheme === "light") { 
+      newTheme = "dark"
+      themeToggleButton.textContent = "🌙";
+    } 
+    else {
+      newTheme = "light"; 
+      themeToggleButton.textContent = "🔆";
+    } 
+
+    SetTheme(newTheme);
   });
 });
